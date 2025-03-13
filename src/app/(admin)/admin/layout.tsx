1525c0c29138ae34ecee6bf4426b9dbb9f660d7d
Ka ThinkLap <trongnghia.ngo@hotmail.com>
2025-03-13 16:01:51 +0700

    re-style

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,26 +1,30 @@
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/auth";
+import { Button } from '@/components/ui/button';
+import { auth } from '@/lib/auth';
+import { Navbar } from '@/app/_components/navbar';
 
 export default async function Layout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const session = await auth();
-	if (!session) notFound();
+  const session = await auth();
+  if (!session) notFound();
 
-	return (
-		<div className="space-y-3">
-			<Button asChild variant="ghost">
-				<Link href="/admin">Profile</Link>
-			</Button>
-			<Button asChild variant="ghost">
-				<Link href="/admin/posts">Posts</Link>
-			</Button>
-			{children}
-		</div>
-	);
+  return (
+    <main>
+      <Navbar />
+      <div className="space-y-3">
+        <Button asChild variant="ghost">
+          <Link href="/admin">Profile</Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/admin/posts">Posts</Link>
+        </Button>
+        {children}
+      </div>
+    </main>
+  );
 }
